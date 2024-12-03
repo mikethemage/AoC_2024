f934b2c4408ea4d8dfc25d02f793071e904b2f22
Mike Dunn <mdunn147@gmail.com>
2024-12-03 09:46:46 +0000

    Day 3 part 2 solved

--- AoC_2024_03/Program.cs
+++ AoC_2024_03/Program.cs
@@ -13,76 +13,100 @@ internal class Program
         string firstNumberText = string.Empty;
         string secondNumberText = string.Empty;
         int total = 0;
+
+        bool enabled = true;
+
         while (position < input.Length)
         {
-            switch (lookingFor)
+            if (enabled)
             {
-                case "mul":
-                    if (input.Length - position > 3 && input.Substring(position, 3) == "mul")
-                    {
-                        position += 3;
-                        lookingFor = "(";
-                    }
-                    else
-                    {
+
+                switch (lookingFor)
+                {
+                    case "mul":
+                        if (input.Length - position > 7 && input.Substring(position, 7) == "don't()")
+                        {
+                            enabled = false;
+                            position += 7;
+                        }
+                        else if (input.Length - position > 3 && input.Substring(position, 3) == "mul")
+                        {
+                            position += 3;
+                            lookingFor = "(";
+                        }
+                        else
+                        {
+                            position++;
+                        }
+                        break;
+
+                    case "(":
+                        if (input[position] == '(')
+                        {
+                            lookingFor = "firstNumber";
+                        }
+                        else
+                        {
+                            lookingFor = "mul";
+                        }
                         position++;
-                    }
-                    break;
+                        break;
 
-                case "(":
-                    if (input[position] == '(')
-                    {
-                        lookingFor = "firstNumber";
-                    }
-                    else
-                    {
-                        lookingFor = "mul";
-                    }
+                    case "firstNumber":
+                        if (input[position] == ',' && firstNumberText.Length > 0)
+                        {
+                            lookingFor = "secondNumber";
+                        }
+                        else if (input[position] >= '0' && input[position] <= '9')
+                        {
+                            firstNumberText += input[position];
+                        }
+                        else
+                        {
+                            firstNumberText = string.Empty;
+                            lookingFor = "mul";
+                        }
+                        position++;
+                        break;
+
+                    case "secondNumber":
+                        if (input[position] == ')' && secondNumberText.Length > 0)
+                        {
+                            int result = int.Parse(firstNumberText) * int.Parse(secondNumberText);
+                            total += result;
+
+                            lookingFor = "mul";
+                            firstNumberText = string.Empty;
+                            secondNumberText = string.Empty;
+                        }
+                        else if (input[position] >= '0' && input[position] <= '9')
+                        {
+                            secondNumberText += input[position];
+                        }
+                        else
+                        {
+                            firstNumberText = string.Empty;
+                            secondNumberText = string.Empty;
+                            lookingFor = "mul";
+                        }
+                        position++;
+                        break;
+
+                    default:
+                        throw new Exception("Invalid expected value!");
+                }
+            }
+            else
+            {
+                if (input.Length - position > 4 && input.Substring(position, 4) == "do()")
+                {
+                    enabled = true;
+                    position += 4;
+                }
+                else
+                {
                     position++;
-                    break;
-
-                case "firstNumber":
-                    if (input[position] == ',' && firstNumberText.Length > 0)
-                    {
-                        lookingFor = "secondNumber";
-                    }
-                    else if (input[position] >= '0' && input[position] <= '9')
-                    {
-                        firstNumberText += input[position];
-                    }
-                    else
-                    {
-                        firstNumberText=string.Empty;
-                        lookingFor = "mul";
-                    }
-                    position++;
-                    break;
-
-                case "secondNumber":
-                    if (input[position] == ')' && secondNumberText.Length > 0)
-                    {
-                        int result = int.Parse(firstNumberText) * int.Parse(secondNumberText);
-                        total += result;
-
-                        lookingFor = "mul";
-                        firstNumberText = string.Empty;
-                        secondNumberText = string.Empty;
-                    }
-                    else if (input[position] >= '0' && input[position] <= '9')
-                    {
-                        secondNumberText += input[position];
-                    }
-                    else
-                    {
-                        firstNumberText=string.Empty;
-                        secondNumberText=string.Empty;
-                        lookingFor = "mul";
-                    }
-                    position++;
-                    break;
-
-                default:
-                    throw new Exception("Invalid expected value!");
+                }
             }
         }
 
