777a3e61b7405642975c0707a91ffb8cb18eb730
Mike Dunn <mdunn147@gmail.com>
2024-12-21 00:34:55 +0000

    Day 20 improved version, still incorrect for part 2 on input

--- AoC_2024_20/Program.cs
+++ AoC_2024_20/Program.cs
@@ -1,266 +1,207 @@
-﻿namespace AoC_2024_20;
+﻿using System.Collections.Generic;
+
+namespace AoC_2024_20;
 
 internal class Program
 {
     static void Main(string[] args)
     {
         //string filename = "sample.txt";
+        //const int improvementsOverOrEqualTo = 1;
+
         string filename = "input.txt";
+        const int improvementsOverOrEqualTo = 100;
+
+        const int maxCheats = 20;       
+        const int improvementsOver = improvementsOverOrEqualTo - 1;
+
         string[] lines = File.ReadAllLines(filename);
         PuzzleData puzzleData = new PuzzleData(lines);
-        const int maxCheats = 20;
 
-        List<State> endStateNoCheating = RunSimulation(puzzleData, new State { Location = puzzleData.Start, History = new() }, -1, false, maxCheats);
+        var currentStep = new Step { Location = puzzleData.Start, StepsTaken = 0 };
 
-        if (endStateNoCheating.Count>0)
+        Dictionary<(int X, int Y), Step> history = new Dictionary<(int X, int Y), Step>();
+        history.Add(puzzleData.Start, currentStep);
+
+        while (currentStep.Location != puzzleData.End)
         {
-            Console.WriteLine($"Shortest path without cheating: {endStateNoCheating[0].StepsTaken}");
+            var nextLocation = (currentStep.Location.X + 1, currentStep.Location.Y);
+            Step? nextStep = GetNextStep(puzzleData, currentStep, history, nextLocation);
 
-
-            Dictionary<((int X, int Y) Start, (int X, int Y) End), int> cheats = new();
-
-            foreach( State cheatStartState in endStateNoCheating[0].History.Values.Where(x=>x.Location!=puzzleData.End) )
+            if (nextStep == null)
             {
-                List<State> intermediateStates = cheatStartState.GetNextStates(puzzleData,true,maxCheats).Where(x=>puzzleData.Walls.Contains(x.Location)).ToList();
-                List<State> cheatEndStates = new List<State>();
-
-                for (int i = 0; i < maxCheats-1; i++)
-                {
-                    List<State> nextIntermediateStates = new List<State>();
-                    foreach (var intermediateState in intermediateStates)
-                    {                        
-                        var temp = intermediateState.GetNextStates(puzzleData, true, maxCheats);
-                        cheatEndStates.AddRange(temp.Where(x => !puzzleData.Walls.Contains(x.Location)));
-                        nextIntermediateStates.AddRange(temp.Where(x => puzzleData.Walls.Contains(x.Location)));
-                    }
-                    intermediateStates = nextIntermediateStates;
-                }
-                
-
-                foreach (var cheatEndState in cheatEndStates)
-                {
-                    if(endStateNoCheating[0].Location==cheatEndState.Location)
-                    {
-                        var improvement = endStateNoCheating[0].StepsTaken - cheatEndState.StepsTaken;
-                        if (improvement > 0)
-                        {
-                            if (cheats.ContainsKey((cheatStartState.Location, cheatEndState.Location)))
-                            {
-                                if (improvement > cheats[(cheatStartState.Location, cheatEndState.Location)])
-                                {
-                                    cheats[(cheatStartState.Location, cheatEndState.Location)] = improvement;
-                                }
-                            }
-                            else
-                            {
-                                cheats.Add((cheatStartState.Location, cheatEndState.Location), improvement);
-                            }
-                        }
-                    }
-
-                    if(endStateNoCheating[0].History.ContainsKey(cheatEndState.Location))
-                    {
-                        var skipToState = endStateNoCheating[0].History[cheatEndState.Location];
-                        var improvement = skipToState.StepsTaken - cheatEndState.StepsTaken;
-
-                        if (improvement > 0)
-                        {
-                            if (cheats.ContainsKey((cheatStartState.Location, cheatEndState.Location)))
-                            {
-                                if(improvement > cheats[(cheatStartState.Location, cheatEndState.Location)])
-                                {
-                                    cheats[(cheatStartState.Location, cheatEndState.Location)] = improvement;
-                                }                                
-                            }
-                            else
-                            {
-                                cheats.Add((cheatStartState.Location, cheatEndState.Location), improvement);
-                            }
-                        }
-                    }
-                }
+                nextLocation = (currentStep.Location.X - 1, currentStep.Location.Y);
+                nextStep = GetNextStep(puzzleData, currentStep, history, nextLocation);
             }
 
-            //Console.WriteLine();
-
-            Dictionary<int,int> countsByImprovment = new Dictionary<int,int>();
-            foreach (var key in cheats.Keys)
+            if (nextStep == null)
             {
-                //Console.WriteLine($"{key}: {cheats[key]}");
-                if(countsByImprovment.ContainsKey(cheats[key]))
-                {
-                    countsByImprovment[cheats[key]]++;
-                }
-                else
-                {
-                    countsByImprovment.Add(cheats[key], 1);
-                }
+                nextLocation = (currentStep.Location.X, currentStep.Location.Y + 1);
+                nextStep = GetNextStep(puzzleData, currentStep, history, nextLocation);
             }
 
-            const int cutoff = 50;
-            Console.WriteLine();
-            int total = 0;
-            foreach (var improvement in countsByImprovment.Keys.Where(x=>x>=cutoff).OrderBy(x=>x))
+            if (nextStep == null)
             {
-                Console.WriteLine($"Improvement of {improvement}: Count={countsByImprovment[improvement]}");
-                total += countsByImprovment[improvement];
+                nextLocation = (currentStep.Location.X, currentStep.Location.Y - 1);
+                nextStep = GetNextStep(puzzleData, currentStep, history, nextLocation);
             }
 
-            Console.WriteLine();
-            Console.WriteLine($"Total: {total}");
+            if (nextStep == null)
+            {
+                throw new Exception("No next step from here!");
+            }
 
-            //int total = 0;
-            //List<State> endStateQuickestCheats = RunSimulation(puzzleData, new State { Location = puzzleData.Start, History = new() }, endStateNoCheating[0].StepsTaken, true);
-            //foreach (var endStateQuickestCheat in endStateQuickestCheats
-            //    .Where(x => x.CheatsUsed > 0 && x.CheatStartLocation != null && x.CheatEndLocation != null)
-            //    .GroupBy(x => (x.CheatStartLocation, x.CheatEndLocation))
-            //    .GroupBy(x=>x.Min(y=>y.StepsTaken))
-            //    .OrderByDescending(x=>x.Key))
-            //{
-
-            //    //Console.WriteLine($"{endStateQuickestCheat.Key.CheatStartLocation?.X},{endStateQuickestCheat.Key.CheatStartLocation?.Y} to {endStateQuickestCheat.Key.CheatEndLocation?.X},{endStateQuickestCheat.Key.CheatEndLocation?.Y}: {endStateQuickestCheat.Min(x=>x.StepsTaken)}");
-            //    Console.WriteLine($"{endStateNoCheating[0].StepsTaken-endStateQuickestCheat.Key}: {endStateQuickestCheat.Count()}");
-
-            //    //Console.WriteLine($"Paths with cheating: {endStateNoCheating[0].StepsTaken-endStateQuickestCheat.Key} steps shorter: {endStateQuickestCheat.Count()}");
-                
-            //    if(endStateNoCheating[0].StepsTaken - endStateQuickestCheat.Key>=100)
-            //    {
-            //        total +=
-            //            endStateQuickestCheat.Count();
-            //    }
-            //}
-
-            //Console.WriteLine($"Total saving at least 100: {total}");
-
+            history.Add(nextStep.Location, nextStep);
+            currentStep = nextStep;
         }
+
+        Console.WriteLine($"Steps taken without cheating: {history[puzzleData.End].StepsTaken}");
+
+        List<int> improvements = new List<int>();
+        foreach (var location in history.Keys)
+        {
+            Dictionary<(int X, int Y), Step> cheets = new Dictionary<(int X, int Y), Step>();
+
+            Dictionary<(int X, int Y), int> placesICanGetTo = GetPlaces(maxCheats, history, location, puzzleData);
+            foreach (var place in placesICanGetTo)
+            {                
+                int improvement = history[place.Key].StepsTaken - (history[location].StepsTaken + place.Value);
+                if (improvement > improvementsOver)
+                {
+                    improvements.Add(improvement);
+                }
+            }
+        }
+
+        int total = 0;
+        foreach (var item in improvements.GroupBy(x=>x).OrderBy(x=>x.Key))
+        {
+            Console.WriteLine($"There are {item.Count()} cheats that save {item.Key} picoseconds.");
+            total += item.Count();
+        }
+        Console.WriteLine();
+        Console.WriteLine($"Total: {total}");
+
     }
 
-    private static List<State> RunSimulation(PuzzleData puzzleData, State startState, int maxSteps, bool cheatingAllowed, int maxCheats)
+    private static Dictionary<(int RemainingSteps, (int X, int Y) Location), Dictionary<(int X, int Y), int>> Memo = new();
+
+    private static Dictionary<(int X, int Y), int> GetPlaces(int remainingSteps, Dictionary<(int X, int Y), Step> history, (int X, int Y) location, PuzzleData puzzleData)
     {
-        List<State> output = new List<State>();
-        PriorityQueue<State, int> priorityQueue = new PriorityQueue<State, int>();
-        priorityQueue.Enqueue(startState, startState.StepsTaken);
-
-        while (priorityQueue.Count > 0)
+        if(Memo.ContainsKey((remainingSteps, location)))
         {
-            //Console.WriteLine($"Queue size: {priorityQueue.Count}");
-            
-            State state = priorityQueue.Dequeue();
+            return Memo[(remainingSteps, location)];
+        }
 
-            //Console.WriteLine($"Head of queue steps taken: {state.StepsTaken}");
-            //Console.WriteLine($"Head of queue Location: {state.Location.X},{state.Location.Y}, cheat count: {state.CheatsUsed}");
-            //Console.ReadKey();
-
-            if (state.CheatsUsed==2 && (
-                state.Location.X<0 ||state.Location.Y<0 || state.Location.X > puzzleData.Walls.Max(w=>w.X) || state.Location.Y > puzzleData.Walls.Max(w=>w.Y)
-                ))
+        Dictionary<(int X, int Y), int> placesICanGetTo = new Dictionary<(int X, int Y), int>();
+        for (int xOffset = -remainingSteps; xOffset <= remainingSteps; xOffset++)
+        {
+            for (int yOffset = -remainingSteps; yOffset <= remainingSteps; yOffset++)
             {
-                continue;
-            }            
-
-            if (maxSteps >= 0 && state.StepsTaken >= maxSteps)
-            {
-                continue;
-            }
-
-            if (state.Location == puzzleData.End)
-            {
-                
-
-                output.Add(state);
-
-                if (maxSteps<0)
+                if ((Math.Abs(xOffset) >= 1 && yOffset == 0) ||
+                    (Math.Abs(yOffset) >= 1) && xOffset == 0)
                 {
-                    break;
+                    (int X, int Y) cheatLocation = (location.X + xOffset, location.Y + yOffset);
+                    if (history.ContainsKey(cheatLocation))
+                    {
+                        int newStepsTaken = Math.Abs(xOffset) + Math.Abs(yOffset);
+
+                        if (placesICanGetTo.ContainsKey(cheatLocation))
+                        {
+                            if (placesICanGetTo[cheatLocation]>newStepsTaken)
+                            {
+                                placesICanGetTo[cheatLocation] = newStepsTaken;
+
+                                if(cheatLocation!=puzzleData.End)
+                                {
+                                    foreach (var place in GetPlaces(remainingSteps - newStepsTaken, history, cheatLocation, puzzleData))
+                                    {
+                                        if (placesICanGetTo.ContainsKey(place.Key))
+                                        {
+                                            if (placesICanGetTo[place.Key] > newStepsTaken + place.Value)
+                                            {
+                                                placesICanGetTo[place.Key] = newStepsTaken + place.Value;
+                                            }
+                                        }
+                                        else
+                                        {
+                                            placesICanGetTo.Add(place.Key, newStepsTaken + place.Value);
+                                        }
+                                    }
+                                }                                
+                            }
+                        }
+                        else
+                        {
+                            placesICanGetTo.Add(cheatLocation, newStepsTaken);
+
+                            if (cheatLocation != puzzleData.End)
+                            {
+                                foreach (var place in GetPlaces(remainingSteps - newStepsTaken, history, cheatLocation, puzzleData))
+                                {
+                                    if (placesICanGetTo.ContainsKey(place.Key))
+                                    {
+                                        if (placesICanGetTo[place.Key] > newStepsTaken + place.Value)
+                                        {
+                                            placesICanGetTo[place.Key] = newStepsTaken + place.Value;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        placesICanGetTo.Add(place.Key, newStepsTaken + place.Value);
+                                    }
+                                }
+                            }                                
+                        }
+                    }
+                    else
+                    {
+                        if(!puzzleData.Walls.Contains(cheatLocation))
+                        {
+                            if(cheatLocation.X>=0 && cheatLocation.X<puzzleData.Width && cheatLocation.Y>=0 && cheatLocation.Y<puzzleData.Height)
+                            {
+                                throw new Exception("Invalid place!");
+                            }                            
+                        }
+                    }
                 }
-                else
-                {
-                    continue;
-                }                   
-            }
-
-            foreach (State nextState in state.GetNextStates(puzzleData, cheatingAllowed, maxCheats))
-            {
-                priorityQueue.Enqueue(nextState, nextState.StepsTaken);
             }
         }
 
-        return output;
+        Memo.Add((remainingSteps, location),placesICanGetTo);
+        return placesICanGetTo;
+    }
+
+    private static Step? GetNextStep(PuzzleData puzzleData, Step currentStep, Dictionary<(int X, int Y), Step> history, (int, int Y) nextLocation)
+    {
+        Step? nextStep = null;
+        if (!history.ContainsKey(nextLocation) && !puzzleData.Walls.Contains(nextLocation))
+        {
+            nextStep = new Step { Location = nextLocation, StepsTaken = currentStep.StepsTaken + 1 };
+        }
+
+        return nextStep;
     }
 }
 
-internal class State
+internal class Step
 {
     public required (int X, int Y) Location { get; set; }
-
-    public (int X, int Y)? CheatStartLocation { get; set; } = null;
-    public (int X, int Y)? CheatEndLocation { get; set; } = null;
-
-    public int StepsTaken
-    {
-        get
-        {
-            return History.Count;
-        }
-    }
-    public int CheatsUsed { get; set; } = 0;
-
-    public required Dictionary<(int X, int Y), State> History { get; init; }
-
-    public List<State> GetNextStates(PuzzleData puzzleData, bool cheatingAllowed, int maxCheats)
-    {
-        List<State> output = new List<State>();
-
-        AddNextState(puzzleData, output, (Location.X + 1, Location.Y), cheatingAllowed, maxCheats);
-        AddNextState(puzzleData, output, (Location.X - 1, Location.Y), cheatingAllowed, maxCheats);
-        AddNextState(puzzleData, output, (Location.X, Location.Y + 1), cheatingAllowed, maxCheats);
-        AddNextState(puzzleData, output, (Location.X, Location.Y - 1), cheatingAllowed, maxCheats);
-
-        return output;
-    }
-
-    private void AddNextState(PuzzleData puzzleData, List<State> output, (int X, int Y) nextLocation, bool cheatingAllowed, int maxCheats)
-    {
-        if (History.ContainsKey(nextLocation))
-        {
-            return;
-        }
-
-        var newHistory = new Dictionary<(int X, int Y), State>(History);
-        newHistory.Add(Location, this);
-        if (puzzleData.Walls.Contains(nextLocation))
-        {
-            if (cheatingAllowed && CheatsUsed < maxCheats - 1)
-            {
-                output.Add(new State { Location = nextLocation, History = newHistory, CheatsUsed = CheatsUsed + 1, CheatStartLocation=CheatStartLocation  ?? Location, CheatEndLocation=null });
-            }
-        }
-        else
-        {
-            int newCheatsUsed = CheatsUsed;            
-            if (CheatsUsed != 0)
-            {
-                newCheatsUsed = CheatsUsed == maxCheats ?  CheatsUsed : CheatsUsed + 1;
-            }
-            
-            var newCheatEndLocation = CheatEndLocation;
-            if(newCheatEndLocation==null && CheatStartLocation!=null)
-            {
-                newCheatEndLocation= nextLocation;
-            }
-
-            output.Add(new State { Location = nextLocation, History = newHistory, CheatsUsed = newCheatsUsed, CheatStartLocation = CheatStartLocation, CheatEndLocation = newCheatEndLocation });
-        }
-    }
+   
+    public required int StepsTaken { get; set; }
 }
 
 internal class PuzzleData
 {
+    public int Width { get; private set; } = 0;
+    public int Height { get; private set; } = 0;
     public (int X, int Y) Start { get; private set; }
     public (int X, int Y) End { get; private set; }
     public HashSet<(int X, int Y)> Walls { get; private set; } = new HashSet<(int X, int Y)>();
     public PuzzleData(string[] lines)
     {
+        Height = lines.Length;
+        Width = lines[0].Length;
         for (int row = 0; row < lines.Length; row++)
         {
             for (int col = 0; col < lines[row].Length; col++)
