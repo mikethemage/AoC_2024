afa1d37074e614db545ab0df339e54f2403699bd
Mike Dunn <mdunn147@gmail.com>
2025-05-04 17:20:57 +0100

    Created blank template for day 22

---
+++ AoC_2024_22/AoC_2024_22/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AoC_2024_22;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        const string filename = "sample.txt";
+        //const string filename = "input.txt";
+
+        string[] lines = File.ReadAllLines(filename);
+        foreach (string line in lines)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
