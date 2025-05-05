d5f656810e254340c2697767288537f43c1b6f58
Mike Dunn <mdunn147@gmail.com>
2025-05-05 10:47:27 +0100

    Day 24 - created blank project

---
+++ AoC_2024_24/AoC_2024_24/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AoC_2024_24;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var lines = File.ReadAllLines("sample.txt");
+        foreach (var line in lines)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
